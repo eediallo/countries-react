27436038c0cfcf5896d0a5364fefb3279a766a93
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-08 09:26:29 +0000

    render countries with summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import { countries } from "./countries.jsx";
 
 function App() {
+  const countriesList = countries.map((country) => {
+    return (
+      <div key={countries.name} className="country-card">
+        <h1>{country.name}</h1>
+        <img src={country.flags.svg} alt="" />
+        <p>
+          <b>Population: </b>
+          {country.population}
+        </p>
+        <p>
+          <b>Region: </b>
+          {country.region}
+        </p>
+        <p>
+          <b>Capital: </b>
+          {country.capital}
+        </p>
+      </div>
+    );
+  });
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Where in the world</h1>
+      {countriesList}
     </>
   );
 }
