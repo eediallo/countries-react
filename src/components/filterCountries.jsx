1a007969904514046c144277adb144fad04cc65f
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-22 11:08:55 +0000

    moves components associated to the search to SearchBar component

--- src/components/filterCountries.jsx
+++ src/components/filterCountries.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
 import { Countries } from "./Countries";
 import PropTypes from "prop-types";
-import SearchBar from "./SearchBar";
-import NumberOfCountriesDisplayed from "./NumberDisplayed";
-import Regions from "./Regions";
-import Section from "./Section";
+import SearchBar from "./SearchBar/SearchBar";
 
 export const FilterCountries = ({ countries }) => {
   // input search filter
@@ -13,42 +10,29 @@ export const FilterCountries = ({ countries }) => {
     countries.sort((a, b) => a.name.common.localeCompare(b.name.common))
   );
 
+  
   let [numberOfCountriesDisplayed, setnumberOfCountriesDisplayed] = useState(0);
-
+  
   useEffect(() => {
     setFilteredCountries(countries);
     setnumberOfCountriesDisplayed(countries.length);
   }, [countries]);
-
-  //drop down filter
   let [filterBy, setFilterBy] = useState("Filter by Region");
-
+  
+  
   return (
     <>
-      <Section
-        className="search-region-filter-container"
-        sectionContainer="section"
-      >
-        <SearchBar
-          countries={countries}
-          setSearchTerm={setSearchTerm}
-          setFilteredCountries={setFilteredCountries}
-          setnumberOfCountriesDisplayed={setnumberOfCountriesDisplayed}
-          searchTerm={searchTerm}
-        />
-        <NumberOfCountriesDisplayed
-          numberOfCountriesDisplayed={numberOfCountriesDisplayed}
-          countries={countries}
-        />
-        <Regions
-          filterBy={filterBy}
-          setFilterBy={setFilterBy}
-          setFilteredCountries={setFilteredCountries}
-          searchTerm={searchTerm}
-          countries={countries}
-          setnumberOfCountriesDisplayed={setnumberOfCountriesDisplayed}
-        />
-      </Section>
+      <SearchBar
+        countries={countries}
+        setSearchTerm={setSearchTerm}
+        setFilteredCountries={setFilteredCountries}
+        setnumberOfCountriesDisplayed={setnumberOfCountriesDisplayed}
+        searchTerm={searchTerm}
+        filterBy={filterBy}
+        setFilterBy={setFilterBy}
+        numberOfCountriesDisplayed={numberOfCountriesDisplayed}
+      />
+
       <Countries countries={filteredCountries} />
     </>
   );
